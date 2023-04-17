26a607b5d07ebd494db4f59ac9bb9e642580aa50
soul <2824687171@qq.com>
2023-04-17 09:35:02 +0800

    creating MyORM.Annotation

---
+++ ShoppingManagement/ShoppingSystem/src/main/java/com/yinjunbiao/MyORM/Annotation/ResultMap.java
@@ -0,0 +1,12 @@
+package com.yinjunbiao.MyORM.Annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ResultMap {
+    String id();
+}
